8a5d44161a3d9051b1b40d88bf545ddccc7d48e8
Worth Winchester <worthhw@gmail.com>
2021-11-21 10:19:52 -0500

    urllib request

--- qrReader.py
+++ qrReader.py
@@ -1,14 +1,17 @@
 import cv2
 import os
 import json
+import urllib.request
 
-img = cv2.imread("static/images/qrImage.png")
+filename = "qrImage.png"
+imgUrl = "http://127.0.0.1:5000/static/images/qrImage.png"
+
+urllib.request.urlretrieve(imgUrl, filename)
+
+img = cv2.imread("qrImage.png")
 detector = cv2.QRCodeDetector()
 detectorData = detector.detectAndDecode(img)
 jsonStr = detectorData[0]
 cmdDict = json.loads(jsonStr)
 
 os.system(cmdDict["cmd"])
-
-#for each in cmdDict["cmdList"]:
-#    os.system(each)
